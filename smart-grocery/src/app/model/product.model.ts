c357199b2a11be5df4e79c6f815fdac08fb60037
Praveen Dias <praveendias1180@gmail.com>
2022-05-20 21:42:53 +0530

    Added the Product model

---
+++ smart-grocery/src/app/model/product.model.ts
@@ -0,0 +1,12 @@
+export interface Product{
+    id: string;
+    productName: string;
+    description: string;
+    category: string;
+    brand: string;
+    expiredDate: string;
+    manufacturedDate: string;
+    createdDate: string;
+    quantity: string;
+    unitPrice: string;
+}
