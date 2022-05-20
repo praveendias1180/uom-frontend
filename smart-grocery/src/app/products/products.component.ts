db38aec34cfed07c5cfd8c4945f756d279db3596
Praveen Dias <praveendias1180@gmail.com>
2022-05-20 14:21:27 +0530

    let product of products

--- smart-grocery/src/app/products/products.component.ts
+++ smart-grocery/src/app/products/products.component.ts
@@ -12,4 +12,23 @@ export class ProductsComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  products = [
+    {
+      id: 1,
+      name: "Rice",
+      description: "White Basmathi Rice imported from Pakistan",
+      created: "Jan 29, 2020",
+      qty: 100,
+      price: "100"
+    },
+    {
+      id: 1,
+      name: "Flour",
+      description: "White Basmathi Flour",
+      created: "Jan 29, 2020",
+      qty: 200,
+      price: "100"
+    }
+  ]
+
 }
