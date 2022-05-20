14925cd03046c13761d7b4fa160159de7076b23b
Praveen Dias <praveendias1180@gmail.com>
2022-05-20 21:43:19 +0530

    addProduct method in ProductService

--- smart-grocery/src/app/service/product.service.ts
+++ smart-grocery/src/app/service/product.service.ts
@@ -1,5 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AddProductResponse } from '../model/add-product-response.model';
+import { Product } from '../model/product.model';
 
 @Injectable({
   providedIn: 'root'
@@ -8,5 +11,13 @@ export class ProductService {
 
   private baseurl = 'http://host1.open.uom.lk:8000'
 
-  constructor(http: HttpClient) { }
+  constructor(private http: HttpClient) { }
+
+  httpOptions = {
+    headers: new HttpHeaders({'Content-Type': 'application/json'})
+  }
+
+  addProduct(product: Product): Observable<AddProductResponse>{
+    return this.http.post<AddProductResponse>(`${this.baseurl}api/product`, product, this.httpOptions)
+  }
 }
