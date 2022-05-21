c04127e6e319e4fac74d1ee9d68f0af5fbcca705
Praveen Dias <praveendias1180@gmail.com>
2022-05-21 10:50:38 +0530

    Viewing products

--- smart-grocery/src/app/view-product/view-product.component.ts
+++ smart-grocery/src/app/view-product/view-product.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../service/product.service';
+import { Product } from '../model/product.model';
 
 @Component({
   selector: 'app-view-product',
   templateUrl: './view-product.component.html',
-  styleUrls: ['./view-product.component.css']
+  styleUrls: ['./view-product.component.css'],
 })
 export class ViewProductComponent implements OnInit {
-
-  constructor() { }
+  public product!: Product;
+  constructor(
+    private productService: ProductService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    let selectedProductId = this.route.snapshot.paramMap.get('id');
+    this.productService.getProductById(selectedProductId).subscribe((res) => {
+      this.product = res.data;
+    });
   }
-
 }
