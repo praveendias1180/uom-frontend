74f85ac0ebbd14531022e8c82e73e6f4e01bb881
Praveen Dias <praveendias1180@gmail.com>
2022-05-21 07:38:58 +0530

    Edit product component logic

--- smart-grocery/src/app/edit-product/edit-product.component.ts
+++ smart-grocery/src/app/edit-product/edit-product.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Product } from '../../Models/product.model';
+import { ProductService } from '../../Services/product.service';
 
 @Component({
   selector: 'app-edit-product',
   templateUrl: './edit-product.component.html',
-  styleUrls: ['./edit-product.component.css']
+  styleUrls: ['./edit-product.component.css'],
 })
 export class EditProductComponent implements OnInit {
+  @Input() product!: Product;
+  isDataUploading = false;
+  @Output() editProductEvent: EventEmitter<void> = new EventEmitter<void>();
+  @Output() cancelEditView: EventEmitter<void> = new EventEmitter<void>();
+  constructor(private productService: ProductService) { }
 
-  constructor() { }
+  ngOnInit(): void { }
 
-  ngOnInit(): void {
+  cancel() {
+    this.cancelEditView.emit();
   }
 
+  onSubmit() {
+    this.isDataUploading = true;
+    this.productService.updateProduct(this.product).subscribe((res) => {
+      this.isDataUploading = false;
+      this.editProductEvent.emit();
+      this.cancelEditView.emit();
+    });
+  }
 }
