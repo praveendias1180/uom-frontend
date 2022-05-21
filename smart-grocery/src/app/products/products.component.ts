baa7e5bb7b359de9c638e6e69cd6c42af9c2a810
Praveen Dias <praveendias1180@gmail.com>
2022-05-21 08:31:18 +0530

    Product component improvements

--- smart-grocery/src/app/products/products.component.ts
+++ smart-grocery/src/app/products/products.component.ts
@@ -17,15 +17,16 @@ export class ProductsComponent implements OnInit {
 
   public showAddProduct: boolean = false;
   public isRowSelected: boolean = false;
-  public rowIndex: number = 0;
+  public rowIndex: number = -1;
   isLoading: boolean = false;
   public products: Product[] = []
   showEditProduct!: boolean;
   selectedProduct!: Product;
 
-  public product_clicked(seletedRow: number){
+  public product_clicked(seletedRow: any, product: Product){
     this.rowIndex = seletedRow
     this.isRowSelected = true
+    this.selectedProduct = product
   }
 
   public showAddProducts(){
@@ -38,8 +39,10 @@ export class ProductsComponent implements OnInit {
 
   getProducts(){
     this.isLoading = true;
-    this.productService.getProducts().subscribe(res => this.products = res.data)
-    this.isLoading = false;
+    this.productService.getProducts().subscribe(res => {
+      this.products = res.data
+      this.isLoading = false;
+    })
   }
 
   refresh(){
