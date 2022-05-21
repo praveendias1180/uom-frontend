26b41b0bdfe6aed52860473d77b591d2fd52e1e3
Praveen Dias <praveendias1180@gmail.com>
2022-05-21 10:50:15 +0530

    Added routes

--- smart-grocery/src/app/app-routing.module.ts
+++ smart-grocery/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
+import { ViewProductComponent } from './view-product/view-product.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {path : 'product' , component: ProductsComponent},
+  {path : 'product-detail-view/:id' , component: ViewProductComponent},
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
